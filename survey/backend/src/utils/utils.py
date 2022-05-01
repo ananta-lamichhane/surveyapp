48b136be57b7c8e8ec68f43d446b639ec537448a
ananta.lamichhane <bloomsnruins.gmail.com>
2022-05-01 23:35:26 +0200

    fixed relative file path issues , working dir based on survey dir

--- survey/backend/src/utils/utils.py
+++ survey/backend/src/utils/utils.py
@@ -41,11 +41,11 @@ def create_item_descritptions(item_id):
         item_id ([int]): ids of the items (from dataset)
     """
     try:
-        movies_file_abspth = os.path.abspath("../data/datasets/movielens_small/movies.csv")
+        movies_file_abspth = os.path.abspath("backend/data/datasets/movielens_small/movies.csv")
         item_labels_df = pd.read_csv(movies_file_abspth, dtype='str')
         #print(item_labels_df)
         
-        links_file_abspath = os.path.abspath("../data/datasets/movielens_small/links.csv")
+        links_file_abspath = os.path.abspath("backend/data/datasets/movielens_small/links.csv")
         imdb_id_links = pd.read_csv(links_file_abspath,dtype='str')
         ## build imdb ids from the imdb <-> omdb <-> movieId relationships extracted from links.csv
         
