c0e1a2498d68b2883a37cb83b6b166cfe6297ac1
ananta.lamichhane <bloomsnruins@gmail.com>
2022-06-04 14:35:46 +0200

    tidy up

--- survey/backend/src/app.py
+++ survey/backend/src/app.py
@@ -47,15 +47,15 @@ def create_app():
   #  from database.models import Survey_Model, Questionnaire_Model
 
     ## import the blueprints for individual routes from routes directory
-    from .routes.questionnaire import questionnaire_bp
-    from .routes.offline_eval import offline_eval_bp
-    from .routes.recommendation import recommendation_bp
-    from .routes.survey import survey_bp
+    from .routes.questionnaire.questionnaire import questionnaire_bp
+    #from .routes.offline_eval import offline_eval_bp
+    from .routes.recommendations.recommendation import recommendation_bp
+    from .routes.survey.survey import survey_bp
 
     ## register blueprints so that they can be accessed as routes to the webapps
     app.register_blueprint(questionnaire_bp)
     app.register_blueprint(survey_bp)
-    app.register_blueprint(offline_eval_bp)
+    #app.register_blueprint(offline_eval_bp)
     app.register_blueprint(recommendation_bp)
 
     ## create the actual tables in the db
