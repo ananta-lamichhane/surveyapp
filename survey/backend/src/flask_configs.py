71f455a42216b971ae29db4d8109f82377a07f0b
ananta-lamichhane <bloomsnruins@gmail.com>
2022-06-05 12:47:22 +0200

    changed flask debug variable to True

--- survey/backend/src/flask_configs.py
+++ survey/backend/src/flask_configs.py
@@ -7,6 +7,7 @@ class Flask_Configs(object):
     CORS_HEADERS = 'Content-Type'
     SQLALCHEMY_DATABASE_URI= "sqlite:///"+"../data/survey_database.db" ## path is current_dir/database/test.db
     SQLALCHEMY_TRACK_MODIFICATIONS= False
+    DEBUG=True
     
     MAIL_SERVER='smtp.gmail.com'
     MAIL_PORT=465
@@ -14,5 +15,6 @@ class Flask_Configs(object):
     MAIL_USE_TLS = False
     MAIL_USERNAME='bloomsnruins@gmail.com'
     MAIL_PASSWORD= ""
+
 
     
